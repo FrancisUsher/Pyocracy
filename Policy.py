624167130b4e4218acd10336a9f19448572ac627
FrancisUsher <francis.w.usher@gmail.com>
2016-07-19 01:50:22 +0200

    Factor out parsing mess into separate file from Policy class. Improve comments and doctstring conformity.

--- Policy.py
+++ Policy.py
@@ -1,54 +1,45 @@
-from Effect import Effect
+from D3Parser import parse_effects_from_row
 
-"""name
-   guiname
-   slider
-   description
-   flags
-   introduce
-   cancel
-   raise
-   lower
-   department
-   mincost
-   maxcost
-   cost multiplier
-   implementation
-   minincome
-   maxincome
-   incomemultiplier
-"""
+
 
 class Policy:
-    def __init__(self, row, heads):
-        self.parse_policy(row, heads)
+    """
 
-    def parse_policy(self, row, heads):
+    Attributes:
+        name (str): Internal unique identifier.
+        guiname (str): How it should be called in-game.
+        slider: 
+        description (str): How it should be described in-game.
+        flags: 
+        introduce: Political capital cost to introduce.
+        cancel: Political capital cost to cancel.
+        raise: Political capital cost to raise.
+        lower: Political capital cost to lower.
+        department: Which policy category this falls under.
+            We might want to extend this to more general policy tags so
+            users can organize the screen in custom views, not just the
+            7 default departments.
+        mincost: Cost to fund policy at minimum.
+        maxcost: Cost to fund policy at maximum.
+        cost multiplier
+        implementation
+        minincome
+        maxincome
+        incomemultiplier
+        effects (list): Formulas for how this policy affects the game situation.
+    """
+    def __init__(self, row, heads):
         """Populate the policy object based on a row of the game Policies CSV.
         Keyword arguments:
             row: Sequence of values from a game Policies CSV row.
             heads: Names for each value in a prefix of the row.
         """
         self.__dict__.update({h:v for (h,v) in zip(heads, row)})
-        self.effects = parse_effects((val for val in split_effects(row)))
-        
-    def parse_effects(effects):
-        for val in effects:
-            if val.strip(): # Don't use any empty effects
-                try:
-                    yield Effect(val)
-                except ValueError:
-                    # Malformed Effect string, ignore it and parse the rest.
-                    # See comment above Effect class for known errors cases.
-                    pass
+        self.effects = parse_effects_from_row(row)
 
-    # returns true if one of the effects, 
     def has_effect(self, search_effects):
+        """Check if the policy affects any of the search_effect tagets."""
         return any(any(term in e.target for e in self.effects)
                    for term in search_effects)
 
-def split_effects(vals):
-    for (i, val) in enumerate(vals[:-1]):
-        if '#effects' in val.lower():
-            return vals[i+1:]
-    return []
+
